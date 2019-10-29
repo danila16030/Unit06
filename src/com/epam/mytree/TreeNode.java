bada26087fdae38cfe8b8334e29ade070293d7e3
danila16030 <danila.chechitov1603@gmail.com>
2019-10-29 21:22:26 +0300

    06

--- src/com/epam/mytree/TreeNode.java
+++ src/com/epam/mytree/TreeNode.java
@@ -11,9 +11,11 @@ public class TreeNode<T> {
         this.right = right;
     }
 
+
     public TreeNode(T data) {
         this.data = data;
     }
+
 
     public T getData() {
         return data;
@@ -37,5 +39,25 @@ public class TreeNode<T> {
 
     public void setRight(TreeNode right) {
         this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TreeNode<?> treeNode = (TreeNode<?>) o;
+
+        if (data != null ? !data.equals(treeNode.data) : treeNode.data != null) return false;
+        if (Left != null ? !Left.equals(treeNode.Left) : treeNode.Left != null) return false;
+        return right != null ? right.equals(treeNode.right) : treeNode.right == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = data != null ? data.hashCode() : 0;
+        result = 31 * result + (Left != null ? Left.hashCode() : 0);
+        result = 31 * result + (right != null ? right.hashCode() : 0);
+        return result;
     }
 }
