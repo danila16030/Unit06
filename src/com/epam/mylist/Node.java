1f4545f256cabcdcea631b64657b23569b964135
danila16030 <danila.chechitov1603@gmail.com>
2019-10-29 21:23:30 +0300

    06

--- src/com/epam/mylist/Node.java
+++ src/com/epam/mylist/Node.java
@@ -29,4 +29,21 @@ public class Node<T> {
         return next;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node<?> node = (Node<?>) o;
+
+        if (data != null ? !data.equals(node.data) : node.data != null) return false;
+        return next != null ? next.equals(node.next) : node.next == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = data != null ? data.hashCode() : 0;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        return result;
+    }
 }
